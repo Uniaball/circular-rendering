425985c78609344468ba382eccb3c0e3e5de274a
Uniaball <2551733616@qq.com>
2026-02-01 15:44:11 +0800

    fix: 不存在的方法

--- src/main/java/com/uniaball/circularrendering/mixin/WorldRendererMixin.java
+++ src/main/java/com/uniaball/circularrendering/mixin/WorldRendererMixin.java
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
     
-    @Inject(method = "shouldSkipCulling", at = @At("HEAD"), cancellable = true)
-    private void onShouldSkipCulling(ChunkPos chunkPos, Vec3i cameraChunk, int renderDistance, 
+    @Inject(method = "isChunkInRenderDistance", at = @At("HEAD"), cancellable = true)
+    private void onIsChunkInRenderDistance(ChunkPos chunkPos, Vec3i cameraChunk, int renderDistance, 
                                      CallbackInfoReturnable<Boolean> cir) {
         CircularRenderingConfig config = CircularRenderingConfig.getInstance();
         
